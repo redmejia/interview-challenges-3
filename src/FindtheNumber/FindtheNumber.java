1c656bb5dca1d9aaafd9c3b5bfaecad58f1bb087
redmejia <reynaldoemejia@gmail.com>
2024-07-23 13:35:50 -0700

    code

--- src/FindtheNumber/FindtheNumber.java
+++ src/FindtheNumber/FindtheNumber.java
@@ -3,13 +3,34 @@ package FindtheNumber;
 import java.util.ArrayList;
 import java.util.Collections;
 
+/*
+Given an array of integers, nums, and a value k, return the kth largest element.
+
+Ex: Given the following array nums…
+
+[1, 2, 3], k = 1, return 3.
+Ex: Given the following array nums…
+
+[9, 2, 1, 7, 3, 2], k = 5, return 2.
+
+Time Complexity:
+Sorting list Collection.sort() sorts the list decending order. The time is O(nlogn) where n is the number of elements in the list.
+
+Space Complexity:
+O(n) collection.sort uses TrimSort which has a space of O(n) temporay storge.
+
+Accessing the k-th Element is O(1)
+
+ */
+
 public class FindtheNumber {
 
-    public static int find(ArrayList<Integer> list, int k) {
+    public static int findNumber(ArrayList<Integer> list, int k) {
 
-       Collections.sort(list, Collections.reverseOrder());
+        Collections.sort(list, Collections.reverseOrder());
 
-        return list.get(k-1);
+        return list.get(k - 1);
+
 
     }
 
@@ -23,8 +44,8 @@ public class FindtheNumber {
             list.add(i);
         }
 
-        find(list, 5);
-
+        System.out.println(findNumber(list, 5));
+        // return 2
 
     }
 
